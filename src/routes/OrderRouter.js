a546ed24b1392a70acc1db165297edcbd11b7620
Vo Van Duy <vovanduy170603@gmail.com>
2024-03-28 12:13:59 +0700

    Sửa một số lỗi

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -6,7 +6,7 @@ const { authUserMiddleWare, authMiddleWare } = require("../middleware/authMiddle
 router.post('/create', authUserMiddleWare, OrderController.createOrder)
 router.get('/get-order-details/:id',authUserMiddleWare, OrderController.getOrderDetails)
 router.get('/get-details-order/:id',authUserMiddleWare, OrderController.getDetailsOrder)
-router.get('/get-details-order-admin/:id', OrderController.getDetailsOrder)
+router.get('/get-details-order-admin/:id', authMiddleWare, OrderController.getDetailsOrder)
 router.put('/update/:id', authMiddleWare, OrderController.updateOrder)
 router.delete('/cancel-order/:id',authUserMiddleWare, OrderController.cancelOrderDetails)
 router.get('/get-all-order', OrderController.getAllOrder)
